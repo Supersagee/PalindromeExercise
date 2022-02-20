26894795e747d2e9313758e0e49a16a26139fcec
Supersagee <williamdsage@gmail.com>
2022-02-19 21:26:02 -0600

    again

--- PalindromeExercise/PalindromeExercise/Program.cs
+++ PalindromeExercise/PalindromeExercise/Program.cs
@@ -4,6 +4,7 @@ namespace PalindromeExercise
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,23 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("kayak", true)]
+        [InlineData("hello", false)]
+        [InlineData("goodbye", false)]
+        public void IsAPalindromeTest(string word, bool expected)
         {
+            var test = new WordSmith();
 
+            bool actual = test.IsAPalindrome(word);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
